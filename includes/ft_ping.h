1dd1bf7d120b89a43905239ff5236b243ba37864
Loupthevenin <loup.thevenin.1@gmail.com>
2025-07-02 14:37:34 +0200

    send_ping

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -4,6 +4,8 @@
 # include "arpa/inet.h"
 # include "netdb.h"
 # include "netinet/in.h"
+# include "netinet/ip.h"
+# include "netinet/ip_icmp.h"
 # include "stdio.h"
 # include "stdlib.h"
 # include "string.h"
@@ -13,17 +15,27 @@
 
 typedef struct s_ping
 {
-	char	*target;
-	char	*ip_str;
-	int		verbose;
-	int		sockfd;
-	int		pid;
-}			t_ping;
+	char		*target;
+	char		*ip_str;
+	int			verbose;
+	int			sockfd;
+	int			pid;
+	int			sequence;
+}				t_ping;
 
-int			parse_ping(int argc, char **argv, t_ping *ping);
-int			resolve_target(t_ping *ping);
+typedef struct s_icmp_echo
+{
+	uint8_t		type;
+	uint8_t		code;
+	uint16_t	checksum;
+	uint16_t	id;
+	uint16_t	sequence;
+}				t_icmp_echo;
+
+int				parse_ping(int argc, char **argv, t_ping *ping);
+int				resolve_target(t_ping *ping);
 
 // Utils;
-void		free_ping(t_ping *ping);
+void			free_ping(t_ping *ping);
 
 #endif
--- srcs/ft_ping.c
+++ srcs/ft_ping.c
@@ -8,6 +8,7 @@ static void	print_ping(t_ping *ping)
 	printf("  verbose: %d\n", ping->verbose);
 	printf("  sockfd: %d\n", ping->sockfd);
 	printf("  pid: %d\n", ping->pid);
+	printf("  sequence: %d\n", ping->sequence);
 }
 
 static void	init_ping(t_ping *ping)
@@ -17,6 +18,7 @@ static void	init_ping(t_ping *ping)
 	ping->verbose = 0;
 	ping->sockfd = 0;
 	ping->pid = 0;
+	ping->sequence = 0;
 }
 
 void	free_ping(t_ping *ping)
@@ -33,6 +35,73 @@ void	free_ping(t_ping *ping)
 	}
 }
 
+static int	create_socket(t_ping *ping)
+{
+	ping->sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	if (!ping->sockfd)
+	{
+		perror("socket");
+		free_ping(ping);
+		exit(1);
+	}
+	return (0);
+}
+
+static uint16_t	checksum(void *data, int len)
+{
+	uint32_t	sum;
+	uint16_t	*ptr;
+
+	sum = 0;
+	ptr = data;
+	while (len > 1)
+	{
+		sum += *ptr++;
+		len -= 2;
+	}
+	if (len == 1)
+		sum += *(uint8_t *)ptr;
+	sum = (sum >> 16) + (sum & 0xFFFF);
+	sum += (sum >> 16);
+	return (~sum);
+}
+
+static void	init_icmp(t_icmp_echo *icmp, int seq, int pid)
+{
+	icmp->type = ICMP_ECHO;
+	icmp->code = 0;
+	icmp->id = htons(pid);
+	icmp->sequence = htons(seq);
+	icmp->checksum = 0;
+	icmp->checksum = checksum(icmp, sizeof(*icmp));
+}
+
+static int	send_ping(t_ping *ping)
+{
+	ssize_t				sent;
+	t_icmp_echo			icmp;
+	struct sockaddr_in	addr;
+
+	init_icmp(&icmp, ping->sequence++, ping->pid);
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr(ping->ip_str);
+	sent = sendto(ping->sockfd, &icmp, sizeof(icmp), 0,
+			(struct sockaddr *)&addr, sizeof(addr));
+	if (sent < 0)
+	{
+		perror("sendto");
+		free_ping(ping);
+		return (1);
+	}
+	return (0);
+}
+
+static int	receive_ping(t_ping *ping)
+{
+	(void)ping;
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	t_ping	ping;
@@ -43,5 +112,12 @@ int	main(int argc, char **argv)
 	if (resolve_target(&ping) != 0)
 		return (1);
 	print_ping(&ping);
+	if (create_socket(&ping) != 0)
+		return (1);
+	// TODO: LOOP HERE;
+	if (send_ping(&ping) != 0)
+		return (1);
+	if (receive_ping(&ping) != 0)
+		return (1);
 	return (0);
 }
